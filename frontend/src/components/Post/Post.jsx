2908d7e5ca98f03a1ae7ec32fd3816aa74beeccf
dorian8x <dorian8x@gmail.com>
2024-06-06 13:25:28 +0100

    posts now display author name instead of their ID

--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -1,16 +1,24 @@
 import ShowComments from "../Comment/ShowComments";
 import CommentButton from "../Comment/CommentButton";
 import Like from "./Like";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { getUserById } from "../../services/users";
 
 const Post = (props) => {
   const user_id = localStorage.getItem("user_id");
+  const [author, setAuthor] = useState("");
   const [isLiked, setLiked] = useState(props.post.like_array.includes(user_id));
   
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    getUserById(token, props.post.author_id)
+      .then((data) => {setAuthor(data.user.fullName)} );
+  }, []);
+
   return (
-    <article /*key={props.post._id}*/>
+    <article>
       {console.log("props is:", props)}
-      {props.post.message}    Likes: {props.post.like_array.length} Posted by: {props.post.author_id}
+      {props.post.message}    Likes: {props.post.like_array.length} Posted by: {author}
       <Like post={props.post} value={isLiked} update={setLiked}/>
       <CommentButton parent={props.post._id} /*value={props.value} update={props.update}*/ />
       <ShowComments parent={props.post._id} /*value={props.value} update={props.update}*/ />
