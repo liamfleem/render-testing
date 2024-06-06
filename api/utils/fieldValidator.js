d6633106c12d2f3391ce32da10f4a7bf757cb0d6
dorian8x <dorian8x@gmail.com>
2024-06-06 08:02:26 +0100

    fixed error in password checker now requires 8 chars or more, as stated in displayed messages

--- api/utils/fieldValidator.js
+++ api/utils/fieldValidator.js
@@ -6,7 +6,7 @@ export const notEmpty = (inputtedText) => {
 
 // executes on submission
 export const passwordValidator = (password) => {
-    const regexLength = /^.{7,}$/;
+    const regexLength = /^.{8,}$/;
     const regexCase = /^(?=.*[a-z])(?=.*[A-Z]).+$/;
     const regexNumber = /^(?=.*\d).+$/;
     if (regexLength.test(password) === false) {
