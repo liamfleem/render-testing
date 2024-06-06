23e3a7b878891e9f873688143610377cbf7e9409
Jack <jackawmurray@hotmail.com>
2024-06-06 10:13:48 +0100

    make name fix

--- frontend/src/components/User/User.jsx
+++ frontend/src/components/User/User.jsx
@@ -3,7 +3,7 @@ import AddFriend from "./AddFriend";
 const User = (props) => {
   return (
     <article key={props.user._id}>
-        {props.user.fullname} <br/>
+        {props.user.fullName} <br/>
         {props.user.email}
         <AddFriend user={props.user} />
     </article>
